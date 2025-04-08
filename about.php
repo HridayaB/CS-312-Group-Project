4199a05896ad535cf44f53761f9bde9a17783b3b
HridayaB <hridayab@boise.cs.colostate.edu>
2025-04-08 11:54:12 -0600

    Hridaya's about page entry

--- about.php
+++ about.php
@@ -1,1 +1,26 @@
-<p>I am working!</p>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>About me</title>
+        <meta name="author" content="Hridaya Bijayananda">
+        <meta name="description" content="Group 29's about page">
+        <meta name="keywords" content="HTML, PHP, Angular, CS-312">
+    </head>
+    <body>
+        <header>
+            <h1>Group 29's About Page</h1>
+        </header>
+        <hr>
+        <h2>Hridaya Bijayananda</h2>
+        <p>
+            Hello, my name is Hridaya Bijayananda. I am currently an undergraduate student pursuing a Bachelor's degree in Computer Science at Colorado State University. 
+            I started out with a concentration in Software Engineering, but currently I am in the general concentration as I wanted to explore more options for myself.
+            I am originally from Nepal and have been in the United States for about ten years.
+        </p>
+        <img height=400 width=300 src="Hridaya's_picture.JPG" alt="Hridaya's picture">
+        <hr>
+        <footer>
+            <p>Authors: Hridaya Bijayananda, </p>
+        </footer>
+    </body>
+</html>
